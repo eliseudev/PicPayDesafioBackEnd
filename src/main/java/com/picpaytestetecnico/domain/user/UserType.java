048c4844fc439488feae2b9258940c066a4e591d
Eliseu Oliveira <eliseu.oliveira@credisis.com.br>
2023-10-24 16:08:03 -0300

    create repository

---
+++ src/main/java/com/picpaytestetecnico/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.picpaytestetecnico.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
---
+++ src/main/java/com/picpaytestetecnico/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.picpaytestetecnico.repositories;
+
+import com.picpaytestetecnico.domain.user.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findUserByDocument(String document);
+    Optional<Users> findUserById(Long Id);
+}
