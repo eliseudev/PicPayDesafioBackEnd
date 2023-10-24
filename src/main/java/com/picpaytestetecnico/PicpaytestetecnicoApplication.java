a2ac6c2c2f50a005cf0e19b0afea452fe27c2ac0
Eliseu Oliveira <eliseu.oliveira@credisis.com.br>
2023-10-24 15:35:58 -0300

    Criando projeto

---
+++ src/main/java/com/picpaytestetecnico/PicpaytestetecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.picpaytestetecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaytestetecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaytestetecnicoApplication.class, args);
+	}
+
+}
